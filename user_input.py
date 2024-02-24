f636ed1902f21e5375cb717d6d7a9797a062fa9f
ericbundi <160766841+ericbundi@users.noreply.github.com>
2024-02-24 22:11:51 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = str(input("name "))
+age = int(input("age "))
+location = str(input("location "))
+sing = "Hello {}, you are {} years old and you are located at {}".format(name,age,location)
+print(sing)
+
